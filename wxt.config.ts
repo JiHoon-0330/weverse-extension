eaa11b18a36938c3c88b556f3ce6950c154a0fee
JiHoon-0330 <ljihoon0330@gmail.com>
2025-07-03 19:55:00 +0900

    사이드패널 설정

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,4 +4,13 @@ import { defineConfig } from "wxt";
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
   outDir: "dist",
+  manifest: {
+    action: {
+      default_title: "Weverse",
+    },
+    side_panel: {
+      default_path: "sidepanel.html",
+    },
+    permissions: ["sidePanel"],
+  },
 });
